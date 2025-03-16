36a7b0a3194448bfe7ca531a5400916a7530dad1
LTT <akaizeroni@gmail.com>
2025-03-16 17:26:41 +0800

    更新线程管理.md 增加一些示例代码，做了说明

--- kernel-sample-0.1.0/thread_sample.c
+++ kernel-sample-0.1.0/thread_sample.c
@@ -22,18 +22,32 @@
 
 static rt_thread_t tid1 = RT_NULL;
 static rt_thread_t tid3 = RT_NULL;
+static rt_thread_t tid5 = RT_NULL;
+
+ALIGN(RT_ALIGN_SIZE)				//对齐
+static char thread2_stack[1024];	//分配线程栈的空间
+static char thread4_stack[1024];
+static struct rt_thread thread2;	//静态定义线程控制块
+static struct rt_thread thread4;
+
 
 /* 线程1的入口函数 */
 static void thread1_entry(void *parameter)
 {
     rt_uint32_t count = 0;
 
+	
     while (1)
     {
 		rt_kprintf("Running thread is: %s ", rt_thread_self()->name);
         /* 线程1采用低优先级运行，一直打印计数值 */
         rt_kprintf(" count: %d\n", count ++);
-        rt_thread_mdelay(500); //延时500ms
+        rt_thread_mdelay(50); //延时50ms
+		if(count%10 == 0)
+		{
+			/*恢复线程3*/
+			rt_thread_resume(tid3);
+		}
     }
 }
 
@@ -42,21 +56,39 @@ static void thread3_entry(void *parameter)
 {
     rt_uint32_t count = 0;
 
-    while (count<100)
+    while (count<=100)
     {	
 		/*rt_thread_self()获取当前运行的线程*/
 		rt_kprintf("Running thread is: %s ",rt_thread_self()->name);
         /* 线程3采用最高优先级运行，打印计数值到100 */
-        rt_kprintf("count: %d\n", count+=10);
-        rt_thread_mdelay(500); //延时500ms
+        rt_kprintf("count: %d\n", count);
+		count+=10;
+		/*延时500 ostick*/
+		//rt_thread_delay(50); 
 		/*线程3让出*/
+		/*挂起线程3*/
+		rt_thread_suspend(tid3);
+		rt_schedule();
+		
+		
+    }
+}
+
+static void thread5_entry(void *parameter)
+{
+    rt_uint32_t count = 0;
+
+    while (count<20)
+    {	
+		/*rt_thread_self()获取当前运行的线程*/
+		rt_kprintf("Running thread is: %s ",rt_thread_self()->name);
+        rt_kprintf("count: %d\n", count+=10);
     }
 }
 
 
-ALIGN(RT_ALIGN_SIZE)				//对齐
-static char thread2_stack[1024];	//分配线程栈的空间
-static struct rt_thread thread2;	//静态定义线程控制块
+
+
 /* 线程2入口 */
 static void thread2_entry(void *param)
 {
@@ -71,13 +103,38 @@ static void thread2_entry(void *param)
 	/*线程3抢占第一 让出 然后线程2抢占*/
 	/*不设置delay 一直运行结束才能让线程1运行*/
 	/*线程1让出后 线程3运行 线程3让出 线程1运行 ---*/
-
+	
     rt_kprintf("thread2 exit\n");
     /* 线程2运行结束后也将自动被系统删除
     (线程控制块和线程栈依然在idle线程中释放) */
 }
 
-/* 删除线程示例的初始化 */
+
+static void thread4_entry(void *param)
+{
+    rt_uint32_t count = 0;
+
+    for (count = 0; count < 10 ; count++)
+    {
+        /* 线程2打印计数值 */
+        rt_kprintf("thread4 count is %d: a\n", count);
+    
+		if (count == 5)
+		{	
+			/*使得线程让出处理器资源*/
+			rt_kprintf("thread4 count is %d: b\n",count);
+			rt_thread_yield();
+			/*执行当前优先级队列的线程5*/
+			/*不会执行线程2 因为线程2已经关闭了*/
+		}
+	}
+
+    rt_kprintf("thread4 exit\n");
+    
+}
+
+
+/* 线程示例的初始化 */
 int thread_sample(void)
 {
     /* 创建动态线程1，名称是thread1，入口是thread1_entry*/
@@ -108,6 +165,26 @@ int thread_sample(void)
 	 /* 如果获得线程控制块，启动这个线程 */
     if (tid3 != RT_NULL)
         rt_thread_startup(tid3);
+	
+	
+	/* 初始化静态线程4，名称是thread4，入口是thread4_entry */
+    rt_thread_init(&thread4,
+                   "thread4",
+                   thread4_entry,
+                   RT_NULL,
+                   &thread4_stack[0],
+                   sizeof(thread4_stack),
+                   THREAD_PRIORITY - 1, THREAD_TIMESLICE);
+    rt_thread_startup(&thread4);
+				   
+	/*创建动态线程5 测试让出资源*/
+	tid5 = rt_thread_create("thread5",
+             thread5_entry, RT_NULL,
+             THREAD_STACK_SIZE,
+             THREAD_PRIORITY - 1, THREAD_TIMESLICE);
+	 /* 如果获得线程控制块，启动这个线程 */
+    if (tid5 != RT_NULL)
+        rt_thread_startup(tid5);
 	
 
     return 0;
